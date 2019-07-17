5cc0a6823fea8a11f908fa17bd27471d58f97d36
DireCorgi <s.frank.ye@gmail.com>
2019-07-16 23:47:05 -0400

    create first migration for restaurants

--- migrations/1563331178890_add-restaurants-table.js
+++ migrations/1563331178890_add-restaurants-table.js
@@ -1,7 +1,25 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-
+  pgm.createTable('restaurants', {
+    id: "id",
+    CAMIS: { type: 'integer', notNull: true },
+    name: { type: 'string', notNull: true },
+    address: { type: 'string' },
+    zipcode: { type: 'string' },
+    phone: { type: 'string' },
+    cuisine_type: { type: 'string' },
+    current_grade: { type: 'string' },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp")
+    },
+  })
+  pgm.createIndex('restaurants', 'CAMIS', { unique: true });
+  pgm.createIndex('restaurants', 'cuisine_type');
+  pgm.createIndex('restaurants', 'current_grade');
+  pgm.createIndex('restaurants', ['cuisine_type', 'current_grade']);
 };
 
 exports.down = (pgm) => {
