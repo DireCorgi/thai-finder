062dbe1ab9044f153760c8458b5bd74e704c28b9
DireCorgi <s.frank.ye@gmail.com>
2019-07-17 16:32:15 -0400

    switch to using database_url to connect for herkou

--- db/index.js
+++ db/index.js
@@ -1,10 +1,4 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
