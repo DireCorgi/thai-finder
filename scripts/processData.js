0e9ddda222b511075266173fb43d82e4739d0a70
DireCorgi <s.frank.ye@gmail.com>
2019-07-17 11:50:37 -0400

    add insertion for inspections

--- scripts/processData.js
+++ scripts/processData.js
@@ -5,6 +5,7 @@ const getData = require('../helpers/getData');
 const batchInsert = require('../db/batchInsert');
 
 let batchInsertRestaurants = {};
+let batchInsertInspections = {};
 
 const processData = async () => {
   let file;
@@ -19,20 +20,34 @@ const processData = async () => {
   fs.createReadStream(file.path)
     .pipe(csv())
     .on('data', row => {
-      if (!batchInsertRestaurants[row.CAMIS] && row['CUISINE DESCRIPTION'] === 'Thai') {
-        batchInsertRestaurants[row.CAMIS] = [
-          row.CAMIS,
-          row.DBA || 'N/A',
-          `${row.BUILDING} ${row.STREET}`,
-          row.ZIPCODE,
-          row.PHONE,
-          row['CUISINE DESCRIPTION']
-        ];
+      if (row['CUISINE DESCRIPTION'] === 'Thai') {
+        if (!batchInsertRestaurants[row.CAMIS]) {
+          batchInsertRestaurants[row.CAMIS] = [
+            row.CAMIS,
+            row.DBA || 'N/A',
+            `${row.BUILDING} ${row.STREET}`,
+            row.ZIPCODE,
+            row.PHONE,
+            row['CUISINE DESCRIPTION']
+          ];
+        }
+        const inspectionKey = `${row.CAMIS}-${row['INSPECTION DATE']}-${row['INSPECTION TYPE']}`;
+        if (!batchInsertInspections[inspectionKey]) {
+          batchInsertInspections[inspectionKey] = [
+            row.CAMIS,
+            row['INSPECTION DATE'],
+            row.grade,
+            row.score,
+            row['GRADE DATE'] || null,
+            row['RECORD DATE'] || null,
+            row['INSPECTION TYPE']
+          ];
+        }
       }
     })
     .on('end', async () => {
-      const queryValues = Object.values(batchInsertRestaurants);
-      console.info(`Finished Processing ${queryValues.length} records; Inserting into DB...`);
+      const restaurants = Object.values(batchInsertRestaurants);
+      console.info(`Finished Processing ${restaurants.length} records; Inserting into DB...`);
       let result;
       try {
         result = await batchInsert({
@@ -49,13 +64,35 @@ const processData = async () => {
               phone = EXCLUDED.phone,
               cuisine_type = EXCLUDED.cuisine_type
           `,
-          batchValues: queryValues
+          batchValues: restaurants
         });
       } catch (error) {
-        console.error('Error Updating Database', error);
+        console.error('Error Updating Database For Restaurants:', error);
         return;
       }
-      console.info('Finished updating/inserting', result.rowCount, 'rows');
+      console.info('Finished updating/inserting restaurants', result.rowCount, 'rows');
+      const inspections = Object.values(batchInsertInspections);
+      try {
+        result = await batchInsert({
+          pool,
+          tableName: 'inspections',
+          columnNames: [
+            'camis',
+            'inspection_date',
+            'grade',
+            'score',
+            'grade_date',
+            'record_date',
+            'inspection_type'
+          ],
+          additionalQuery: 'ON CONFLICT (camis, inspection_date, inspection_type) DO NOTHING',
+          batchValues: inspections
+        });
+      } catch (error) {
+        console.error('Error Updating Database For Inspections:', error);
+        return;
+      }
+      console.info('Finished updating/inserting inspections', result.rowCount, 'rows');
       // console.info('deleting file...');
       // fs.unlinkSync(file.path);
       // console.info('delete completed');
